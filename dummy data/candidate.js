46433e7350b13054d003247bd0f007eccc5a41b0
unknown <your-email@example.com>
2025-08-17 01:57:12 +0530

    final

---
+++ dummy data/candidate.js
@@ -0,0 +1,52 @@
+[
+  {
+    "name": "Narendra Modi",
+    "party": "BJP",
+    "age": 71,
+    "Area_Standing_election": "Varanasi",
+    "address": "Varanasi, Uttar Pradesh",
+    "aadharCardNumber": "123456789012",
+    "email": "modi@bjp.in",
+    "mobile": "9876543210"
+  },
+  {
+    "name": "Rahul Gandhi",
+    "party": "INC",
+    "age": 51,
+    "Area_Standing_election": "Wayanad",
+    "address": "Wayanad, Kerala",
+    "aadharCardNumber": "234567890123",
+    "email": "rahul@inc.in",
+    "mobile": "9876543211"
+  },
+  {
+    "name": "Arvind Kejriwal",
+    "party": "AAP",
+    "age": 53,
+    "Area_Standing_election": "New Delhi",
+    "address": "New Delhi, Delhi",
+    "aadharCardNumber": "345678901234",
+    "email": "kejriwal@aap.in",
+    "mobile": "9876543212"
+  },
+  {
+    "name": "Mamata Banerjee",
+    "party": "AITC",
+    "age": 67,
+    "Area_Standing_election": "Bhabanipur",
+    "address": "Kolkata, West Bengal",
+    "aadharCardNumber": "456789012345",
+    "email": "mamata@aitc.in",
+    "mobile": "9876543213"
+  },
+  {
+    "name": "Yogi Adityanath",
+    "party": "BJP",
+    "age": 49,
+    "Area_Standing_election": "Gorakhpur",
+    "address": "Gorakhpur, Uttar Pradesh",
+    "aadharCardNumber": "567890123456",
+    "email": "yogi@bjp.in",
+    "mobile": "9876543214"
+  }
+]
